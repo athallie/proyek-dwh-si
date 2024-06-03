61d47e47702eafb545daab727b906d41fbb9885d
eavelie <athallahnaufalr@gmail.com>
2024-06-03 15:59:31 +0700

    updated time etl and workflow

--- sql-scripts/data-warehouse-ddl.sql
+++ sql-scripts/data-warehouse-ddl.sql
@@ -12,25 +12,11 @@ CREATE TABLE IF NOT EXISTS gravity_books_dwh.customer (
 );
 
 CREATE TABLE IF NOT EXISTS gravity_books_dwh.time (
-    time_sk SERIAL UNIQUE PRIMARY KEY,
+    time_sk SERIAL PRIMARY KEY,
     year INT,
     month INT,
     day INT
 );
-SET @@cte_max_recursion_depth = 1500;
-INSERT INTO time (year, month, day)
-WITH RECURSIVE DateSequence AS (
-    SELECT '2021-01-01' AS date
-    UNION ALL
-    SELECT DATE_ADD(date, INTERVAL 1 DAY)
-    FROM DateSequence
-    WHERE date < '2024-12-31'
-)
-SELECT
-    YEAR(date) AS year,
-    MONTH(date) AS month,
-    DAY(date) AS day
-FROM DateSequence;
 
 CREATE TABLE IF NOT EXISTS gravity_books_dwh.shipping_method (
     method_id INT PRIMARY KEY,
